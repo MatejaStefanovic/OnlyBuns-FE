485ba73f67a93cd4794ac08bd48dbd04155b5751
jelena-ra <jelena.rdsc02@gmail.com>
2024-11-11 12:52:59 +0100

    fixed minor visual bugs

--- onlybuns-fe/src/components/navbar/navbar.js
+++ onlybuns-fe/src/components/navbar/navbar.js
@@ -14,11 +14,13 @@ const NavBar = () => {
             <React.Fragment>
               <div className="nav-item"><Link className="nav-item" to="/profile">PROFILE</Link></div>
               <div className="nav-item" onClick={logout}><Link className="nav-item">LOG OUT</Link></div>
+              <div className="nav-item"><Link className="nav-item" to="post">CREATE POST</Link></div>
             </React.Fragment>
           ) : (
             <React.Fragment>
               <div className="nav-item"><Link className="nav-item" to="/login">LOG IN</Link></div>
               <div className="nav-item"><Link className="nav-item" to="/signup">SIGN UP</Link></div>
+              <div className="nav-item"><Link className="nav-item" to="post">CREATE POST</Link></div>
             </React.Fragment>
           )}
         </div>
