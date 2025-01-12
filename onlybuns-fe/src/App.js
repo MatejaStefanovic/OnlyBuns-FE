a758409a9cb8af7afc27fd423fb50a8d0fa48d48
unknown <matejastefanovic214@gmail.com>
2025-01-12 16:25:20 +0100

    feat: trends

--- onlybuns-fe/src/App.js
+++ onlybuns-fe/src/App.js
@@ -15,6 +15,7 @@ import TestComponent from './pages/test';
 import PostsView from './pages/postsView/posts-view'
 import EditPost from './pages/editPosts/editPosts';
 import LoggedInUserProfile from './pages/user/loggedInUserProfile';
+import TrendsPage from './pages/trends/trends';
 function App() {
 
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="signup" element={<SignUp />} />
           <Route path="explore" element={<PostsView />} />
           <Route path="edit" element={<EditPost />} />
+          <Route path="trends" element={<TrendsPage />} />
         </Route>
         <Route path="myProfile" element={<LoggedInUserProfile />} />
         <Route path="/profile/:username" element={<ProfilePage />} />
